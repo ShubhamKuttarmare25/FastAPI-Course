41f471469c2f8e831c484e323aca71c50bcc4c1a
ShubhamKuttarmare25 <shubhamkuttarmare8329@gmail.com>
2025-08-11 12:51:44 +0530

    added comments

--- Project3/database.py
+++ Project3/database.py
@@ -6,11 +6,18 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db' # this will be automatically created no need to explicitely create this 
 
+#create engine to run application
+#for sqlite, we use sqlite:///./<filename> format
+#sqlite will allow only one connection at a time, so we need to set check_same_thread to False
+#each thread will handle indipendent requests
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {'check_same_thread': False})
 
-
+#create a session local class to create session for each request
+#autocommit and autoflush are set to False to control transaction manually ohterwise it will commit automatically
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
-
+#create a base class for declarative models
+#this will be used to create tables in the database
+#all models will inherit from this base class
 Base = declarative_base()
 
--- Project3/models.py
+++ Project3/models.py
@@ -4,7 +4,7 @@ from database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
 
-
+#this is a model for todos table
 class Todos(Base):
     __tablename__ = 'todos'
     id = Column(Integer, primary_key = True, index = True)
